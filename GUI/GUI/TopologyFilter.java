e26d55e376e54f7fba852309765c80087481aff8
unly <leongrch@gmail.com>
2018-03-31 17:31:32 +0200

    moved GUI files to package directory
    moved files to satisfy module and package hierarchy, format code and optimze imports

--- GUI/GUI/TopologyFilter.java
+++ GUI/GUI/TopologyFilter.java
@@ -26,130 +26,127 @@
 package GUI;
 
 import java.io.File;
-import javax.swing.*;
-import javax.swing.filechooser.*;
 
 public final class TopologyFilter {
-  
-  public static BRITEFilter brite = new BRITEFilter();
-  public static NLANRFilter nlanr = new NLANRFilter();
-  public static GTITMFilter gtitm = new GTITMFilter();
-  public static GTTSFilter gtts = new GTTSFilter();
-  public static InetFilter inet = new InetFilter();
-  public static SkitterFilter skitter = new SkitterFilter();
-  public static ScanFilter scan = new ScanFilter();
+
+    public static BRITEFilter brite = new BRITEFilter();
+    public static NLANRFilter nlanr = new NLANRFilter();
+    public static GTITMFilter gtitm = new GTITMFilter();
+    public static GTTSFilter gtts = new GTTSFilter();
+    public static InetFilter inet = new InetFilter();
+    public static SkitterFilter skitter = new SkitterFilter();
+    public static ScanFilter scan = new ScanFilter();
 }
 
 final class BRITEFilter extends javax.swing.filechooser.FileFilter {
-    
+
     public boolean accept(File f) {
-	if (f.isDirectory())
-	    return true;
-       
-	String fname = f.getName();
-	String ext = fname.substring(fname.lastIndexOf('.')+1);
-	if (ext!=null) {
-	    if (ext.equals("brite"))
-		return true;
-	    else return false;
-	}
-	return false;
+        if (f.isDirectory())
+            return true;
+
+        String fname = f.getName();
+        String ext = fname.substring(fname.lastIndexOf('.') + 1);
+        if (ext != null) {
+            if (ext.equals("brite"))
+                return true;
+            else return false;
+        }
+        return false;
     }
 
     public String getDescription() {
-	return "BRITE topologies (*.brite)";
+        return "BRITE topologies (*.brite)";
     }
-    
+
 }
 
 
 final class NLANRFilter extends javax.swing.filechooser.FileFilter {
-    
+
     public boolean accept(File f) {
-	if (f.isDirectory())
-	    return true;
-       
-	String fname = f.getName();
-	String ext = fname.substring(fname.lastIndexOf('.')+1);
-	if (ext!=null) {
-	    if (ext.equals("nlanr"))
-		return true;
-	    else return false;
-	}
-	return false;
+        if (f.isDirectory())
+            return true;
+
+        String fname = f.getName();
+        String ext = fname.substring(fname.lastIndexOf('.') + 1);
+        if (ext != null) {
+            if (ext.equals("nlanr"))
+                return true;
+            else return false;
+        }
+        return false;
     }
 
     public String getDescription() {
-	return "NLANR topologies (*.nlanr)";
+        return "NLANR topologies (*.nlanr)";
     }
-    
+
 }
 
 
 final class SkitterFilter extends javax.swing.filechooser.FileFilter {
-    
+
     public boolean accept(File f) {
-	if (f.isDirectory())
-	    return true;
-       
-	String fname = f.getName();
-	String ext = fname.substring(fname.lastIndexOf('.')+1);
-	if (ext!=null) {
-	    if (ext.equals("skitter"))
-		return true;
-	    else return false;
-	}
-	return false;
+        if (f.isDirectory())
+            return true;
+
+        String fname = f.getName();
+        String ext = fname.substring(fname.lastIndexOf('.') + 1);
+        if (ext != null) {
+            if (ext.equals("skitter"))
+                return true;
+            else return false;
+        }
+        return false;
     }
 
     public String getDescription() {
-	return "Skitter Artsdump (*.skitter)";
+        return "Skitter Artsdump (*.skitter)";
     }
-    
+
 }
 
 
 final class ScanFilter extends javax.swing.filechooser.FileFilter {
-    
+
     public boolean accept(File f) {
-	if (f.isDirectory())
-	    return true;
-       
-	String fname = f.getName();
-	String ext = fname.substring(fname.lastIndexOf('.')+1);
-	if (ext!=null) {
-	    if (ext.equals("scan"))
-		return true;
-	    else return false;
-	}
-	return false;
+        if (f.isDirectory())
+            return true;
+
+        String fname = f.getName();
+        String ext = fname.substring(fname.lastIndexOf('.') + 1);
+        if (ext != null) {
+            if (ext.equals("scan"))
+                return true;
+            else return false;
+        }
+        return false;
     }
 
     public String getDescription() {
-	return "SCAN snapshots (*.scan)";
+        return "SCAN snapshots (*.scan)";
     }
-    
-}
 
+}
 
 
 final class GTITMFilter extends javax.swing.filechooser.FileFilter {
     public boolean accept(File f) {
-	if (f.isDirectory())
-	    return true;
-	
-	String fname = f.getName();
-	String ext = fname.substring(fname.lastIndexOf('.')+1);
-	if (ext!=null) {
-	    if (ext.equals("gtitm"))
-		return true;
-	    else return false;
-	}
-	return false;
+        if (f.isDirectory())
+            return true;
+
+        String fname = f.getName();
+        String ext = fname.substring(fname.lastIndexOf('.') + 1);
+        if (ext != null) {
+            if (ext.equals("gtitm"))
+                return true;
+            else return false;
+        }
+        return false;
     }
-    
+
     public String getDescription() {
-	return "GTITM topologies (*.gtitm)";
+        return "GTITM topologies (*.gtitm)";
     }
 
 
@@ -157,42 +154,42 @@ final class GTITMFilter extends javax.swing.filechooser.FileFilter {
 
 final class GTTSFilter extends javax.swing.filechooser.FileFilter {
     public boolean accept(File f) {
-	if (f.isDirectory())
-	    return true;
-	
-	String fname = f.getName();
-	String ext = fname.substring(fname.lastIndexOf('.')+1);
-	if (ext!=null) {
-	    if (ext.equals("gtts"))
-		return true;
-	    else return false;
-	}
-	return false;
+        if (f.isDirectory())
+            return true;
+
+        String fname = f.getName();
+        String ext = fname.substring(fname.lastIndexOf('.') + 1);
+        if (ext != null) {
+            if (ext.equals("gtts"))
+                return true;
+            else return false;
+        }
+        return false;
     }
-    
+
     public String getDescription() {
-	return "GTITM transit-stub (*.gtts)";
+        return "GTITM transit-stub (*.gtts)";
     }
 }
 
 
 final class InetFilter extends javax.swing.filechooser.FileFilter {
     public boolean accept(File f) {
-	if (f.isDirectory())
-	    return true;
-	
-	String fname = f.getName();
-	String ext = fname.substring(fname.lastIndexOf('.')+1);
-	if (ext!=null) {
-	    if (ext.equals("inet"))
-		return true;
-	    else return false;
-	}
-	return false;
+        if (f.isDirectory())
+            return true;
+
+        String fname = f.getName();
+        String ext = fname.substring(fname.lastIndexOf('.') + 1);
+        if (ext != null) {
+            if (ext.equals("inet"))
+                return true;
+            else return false;
+        }
+        return false;
     }
-    
+
     public String getDescription() {
-	return "Inet topologies (*.inet)";
+        return "Inet topologies (*.inet)";
     }
 
 
